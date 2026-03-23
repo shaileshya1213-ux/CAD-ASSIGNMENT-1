a73886badccfe7bd808e88a83635037e927cdb6f
shaileshya1213-ux <shaileshya1213@gmail.com>
2026-03-23 10:30:24 +0530

    Add files via upload

---
+++ sub.py
@@ -0,0 +1,6 @@
+def subtract(a, b):
+    return a - b
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+result = subtract(num1, num2)
+print("Difference =", result)
