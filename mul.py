857970d46937094cb17932ea91296fe258434108
shaileshya1213-ux <shaileshya1213@gmail.com>
2026-03-23 10:33:53 +0530

    Add files via upload

---
+++ mul.py
@@ -0,0 +1,6 @@
+def multiply(a, b):
+    return a * b
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+result = multiply(num1, num2)
+print("Product =", result)
